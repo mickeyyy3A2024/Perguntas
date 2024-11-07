c1d74b7a251abfede642bfc66ea59f3352071a87
mickeyyy3A2024 <00001092161521sp@al.educacao.sp.gov.br>
2024-11-07 10:13:02 -0300

    Perguntas

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Biologia',
+    'O que é mitocondria e para que serve?',
+    'Organela presente em todas as células, sua função é gerar energia para a célula.'
+)
+
+criaCartao(
+    'Geografia',
+    'O que é relevo?',
+    'é o conjunto de feições da superfície terrestre'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é a raíz quadrada de 81?',
+    '9.9=81, então a raíz quadrada de 81 é 9'
+)
+
+criaCartao(
+    'Biologia',
+    'Quais são as 4 fases da mitose?',
+    'Prófase, metáfase, anáfase, telófase,'
+)
